e80849833051eb7ecbee0e9d5ff9ab47599b89d0
AidanDai <aidandai@126.com>
2016-10-07 21:02:13 +0800

    adjust main menu

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('build', shell.task(['jekyll build --watch']));
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
     // Reloads page when some of the already built files changed:
-    gulp.watch('./asset/style/dev/*.css', ["css"]);
+    gulp.watch('./asset/style/dev/*.css', ['css']);
     gulp.watch('./_site/**/*.*').on('change', browserSync.reload);
 });
 
