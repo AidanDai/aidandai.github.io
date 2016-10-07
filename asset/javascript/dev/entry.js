b7ac86ffbcb0955f049782af50a51a593d0c0d34
AidanDai <aidandai@126.com>
2016-10-07 11:24:48 +0800

    manage project direction

--- asset/javascript/dev/entry.js
+++
@@ -1,3 +0,0 @@
-var homeBackgroundImage = require("./home-background-image");
-
-homeBackgroundImage();
--- asset/javascript/dev/home-background-image.js
+++
@@ -1,7 +0,0 @@
-function homeBackgroundImage(){
-	var home = document.querySelector(".home-contain");
-
-	home.style.backgroundImage = `linear-gradient(to right, ${randomColor()} 0%, ${randomColor()} 100%)`;
-}
-
-module.exports = homeBackgroundImage;
