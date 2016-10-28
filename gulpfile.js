b3dcee7355692899d7043afbf64320a047c1d459
AidanDai <aidandai@126.com>
2016-10-28 17:58:16 +0800

    start using less

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
-/**
- * precss and package css
- * @file webpack.config.js
- * @author Aidan
- * @mail webaidandai@gmail.com
- */
 let gulp = require('gulp'),
 
-  postcss = require('gulp-postcss'),
-  rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
+  less = require('gulp-less'),
+  path = require('path'),
 
-  precss = require('precss'),
-  autoprefixer = require('autoprefixer'),
+  // postcss = require('gulp-postcss'),
+  // rename = require('gulp-rename'),
+  // concat = require('gulp-concat'),
+
+  // precss = require('precss'),
+  // autoprefixer = require('autoprefixer'),
 
   shell = require('gulp-shell');
   browserSync = require('browser-sync').create();
 
-gulp.task('css', function () {
-    let processors = [
-        precss(),
-        autoprefixer({browsers: ['last 1 version']})
-    ];
-
-    return gulp.src('./asset/style/dev/*.css')
-        .pipe(postcss(processors))
-        .pipe(concat('index.css'))
-        .pipe(gulp.dest('./asset/style/'));
+gulp.task('less', function () {
+  return gulp.src('./asset/style/dev-less/index.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./asset/style'));
 });
+
+// gulp.task('css', function () {
+//     let processors = [
+//         precss(),
+//         autoprefixer({browsers: ['last 1 version']})
+//     ];
+
+//     return gulp.src('./asset/style/dev/*.css')
+//         .pipe(postcss(processors))
+//         .pipe(concat('index.css'))
+//         .pipe(gulp.dest('./asset/style/'));
+// });
 
 // Task for building blog when something changed:
 gulp.task('build', shell.task(['jekyll build --watch']));
@@ -35,8 +40,8 @@ gulp.task('build', shell.task(['jekyll build --watch']));
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
     // Reloads page when some of the already built files changed:
-    gulp.watch('./asset/style/dev/*.css', ['css']);
+    gulp.watch('./asset/style/dev-less/*.less', ['less', browserSync.reload]);
     gulp.watch('./_site/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['css', 'build', 'serve']);
+gulp.task('default', ['less', 'build', 'serve']);
