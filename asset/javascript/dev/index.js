118b80cfcdfe9d831a91c1e30ece0450d4af419a
AidanDai <aidandai@126.com>
2019-06-16 16:55:29 +0800

    fix: sw 入口文件

--- asset/javascript/dev/index.js
+++ asset/javascript/dev/index.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 if ('serviceWorker' in navigator) {
 	// Use the window load event to keep the page load performant
-	window.addEventListener('load', () => {
+	window.addEventListener('load', function serviceWorkerRegister(){
 		navigator.serviceWorker.register('/assets/javascript/service-worker.js');
 	});
 }
